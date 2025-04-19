54d7761f26f1287c4d9dc62f6ff3bb6cfaa190e9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-19 12:14:13 +0100

    commit1

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -14,7 +14,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    ReactiveFormsModule // Importation de ReactiveFormsModule ici
   ],
   declarations: [Tab1Page]
 })
