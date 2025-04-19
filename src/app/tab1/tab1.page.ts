4cc8ecd7173a19049d9307af70f7038a165f81b6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-19 12:09:58 +0100

    commit1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   standalone: false,
 })
 export class Tab1Page implements OnInit {
+  myForm: FormGroup;
 
   constructor() {}
 
