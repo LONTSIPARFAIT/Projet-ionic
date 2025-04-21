dc03e357ef3736a6c7cb4b8c381a92a1670a07ba
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-21 14:45:58 +0100

    commit1

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { EditSubmissionModalComponent } from './tab1.page'; // Importer le composant inline
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
