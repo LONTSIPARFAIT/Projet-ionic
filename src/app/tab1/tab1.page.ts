8e0206e5352b16460d39758c1ea7a58efc1476e5
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-21 14:50:06 +0100

    commit1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -250,7 +250,9 @@ export class Tab1Page implements OnInit {
         </ion-button>
       </form>
     </ion-content>
-  `
+  `,
+  standalone: true, // Marquer explicitement comme standalone
+  imports: [CommonModule, IonicModule, ReactiveFormsModule] /
 })
 
 export class EditSubmissionModalComponent {
