44ff228dd148e442db3aeab4acb154e8d6c26a40
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-04-24 09:47:34 +0100

    commit1

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,30 +1,30 @@
 
 
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ApiService {
-  private apiUrl = 'http://localhost/contact_api/index.php';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class ApiService {
+//   private apiUrl = 'http://localhost/contact_api/index.php';
 
-  constructor(private http: HttpClient) {}
+//   constructor(private http: HttpClient) {}
 
-  submitForm(data: any): Observable<any> {
-    return this.http.post(this.apiUrl, data);
-  }
+//   submitForm(data: any): Observable<any> {
+//     return this.http.post(this.apiUrl, data);
+//   }
 
-  getSubmissions(): Observable<any> {
-    return this.http.get(this.apiUrl);
-  }
+//   getSubmissions(): Observable<any> {
+//     return this.http.get(this.apiUrl);
+//   }
 
-  updateSubmission(data: any): Observable<any> {
-    return this.http.put(this.apiUrl, data);
-  }
+//   updateSubmission(data: any): Observable<any> {
+//     return this.http.put(this.apiUrl, data);
+//   }
 
-  deleteSubmission(id: number): Observable<any> {
-    return this.http.delete(this.apiUrl, { body: { id } });
-  }
-}
+//   deleteSubmission(id: number): Observable<any> {
+//     return this.http.delete(this.apiUrl, { body: { id } });
+//   }
+// }
